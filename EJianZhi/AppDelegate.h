cc1e385db69409bb8bf50853964ac9ecb4b8df28
RAY <srtongjitu@163.com>
2014-12-23 14:20:33 +0800

    Initial Commit

---
+++ EJianZhi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EJianZhi
+//
+//  Created by RAY on 14/12/23.
+//  Copyright (c) 2014年 麻辣工作室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EJianZhi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EJianZhi
+//
+//  Created by RAY on 14/12/23.
+//  Copyright (c) 2014年 麻辣工作室. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
